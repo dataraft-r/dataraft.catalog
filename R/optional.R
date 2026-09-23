5ea688b3dc7a19384c75148f1d2efd298c92620b
Jan-Hendrik Weinert <65009403+JanWein@users.noreply.github.com>
2026-09-23 08:31:48 +0200

    Implement coordinated deep review changes

--- R/optional.R
+++
@@ -1,9 +0,0 @@
-optional_dbt <- function(name) {
-  dataraft.core::dr_internal_need("dataraft.dbt", "This catalog operation")
-  getExportedValue("dataraft.dbt", name)
-}
-
-optional_lake <- function(name) {
-  dataraft.core::dr_internal_need("dataraft.lake", "This catalog operation")
-  getExportedValue("dataraft.lake", name)
-}
--- tests/testthat/test-exports.R
+++
@@ -1,8 +0,0 @@
-test_that("component attachment exports only prefixed names", {
-  exports <- getNamespaceExports("dataraft.catalog")
-  expect_true(all(startsWith(exports, "dr_")))
-  expect_length(
-    intersect(exports, c("ident", "exec", "abort", "now", "%||%")),
-    0L
-  )
-})
